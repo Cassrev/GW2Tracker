4a53e4feeac92b90f711559a04d78b8b99c889a0
Cassie <classierev@gmail.com>
2023-02-16 14:11:09 -0600

    Separate Tiers complete

--- src/components/materialList/matlist.js
+++ src/components/materialList/matlist.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react"
 
 export const MatList = () => {
-    const [material, setMaterials] = useState([])
+    const [materials, setMaterials] = useState([])
     const [tiers, setTiers] = useState([])
-    const [filteredTiers, setFilterTier] = useState(material)
+    const [filteredTiers, setFilterTier] = useState([])
     
 
-const getAllMaterials = () => {
+    const getAllMaterials = () => {
         fetch(`http://localhost:8088/materials`)
             .then(response => response.json())
             .then((matArray) => {
@@ -18,60 +18,73 @@ const getAllMaterials = () => {
         fetch(`http://localhost:8088/tiers`)
             .then(response => response.json())
             .then((tiertArray) => {
-                setMaterials(tiertArray)
+                setTiers(tiertArray)
             })
     }
 
+    const filterMat = (tierId) => {
+        return materials.filter(material => matchTiersMat(material, tierId))
+    
+    }
+
+
+    const matchTiersMat = (material, tierId ) => {
+        return ( 
+            material.tierId === tierId
+        )
+    }
+
+    const renderTier = (tierId) => {
+        if(tiers.length > 0 && materials.length > 0){
+            return (
+            <>
+            <h1>Tier {tierId}</h1>
+            <div>{dropDown(filterMat(tierId))}</div>
+            </>
+            )
+        } else {
+            return "No list"
+        }
+    };
+
+ 
     useEffect(
         () => {
-            getAllMaterials()
+            getAllMaterials();
+            getAllTiers();
            
-            console.log("Initial state of tickets", material) // View the initial state of tickets
-        },
+            },
         [] // When this array is empty, you are observing initial component state
     )
 
     let handleMatChange = (e) => {
     setMaterials(e.target.value);
-  };
+    };
 
-  const filterTiers = (tiers) => {
-     setFilterTier(
-        material.find(mat => {
-            return ( 
-                mat.tierId === tiers.id
-            )
-        })
-     )
-  }
-    return (
-        <>
-    
+const dropDown = (materials) => {
+    return (<select onChange={handleMatChange}>
+                {materials.map(material => <option value={material.id}>{material.matName}</option>)}             
 
-   <div>
-<h2>Tier 1</h2>
-<div className = "Materials">
-     <select onChange={handleMatChange}>
-        
-                    <option value="Select Material">Select Mat</option>
-              {material.map((matObj) => (
-              <option value={matObj.id === filterTiers}>{matObj.matName}</option>
-              ))}
-
-     </select>
-</div>
-   </div>
-</>
- );
-
-
-
-
-    
+            </select>
+    )
 }
 
+
+return (
+    <>
+        <div>
+            {renderTier(1)}
+            {renderTier(2)}
+            {renderTier(3)}
+            {renderTier(4)}
+            {renderTier(5)}
+            {renderTier(6)}
+        </div>
+    </>
+);
+}
+
+// ALWAYS BREAK THINGS UP IN INDIVIDUAL FUNCTIONS
+// Get better at naming stuff
 // Algorithmic thinking(writing steps down)
 // Create dropdown html for tiers
-// Use .map array method to render html for each tier object.
-// Use .map Array method to render html for material object.
-
